2e046f0dc23c8dca73cf299af57e21d81c448ec6
marysgreenwood <marysgreenwood@gmail.com>
2023-02-28 20:51:57 -0500

    added list component

---
+++ client/src/components/list/index.js
@@ -0,0 +1,23 @@
+//imports
+
+
+export default function List () {
+    //API Call
+    return (
+        // map through return data
+        // listData.map(item) item.packItemName, item.room
+        <>
+        <h3>Packing List</h3>
+        <ul>
+    
+            <li>Hello</li>
+            <li>World</li>
+            <li>we made a list!</li>
+            
+        </ul>
+       </>
+
+    )
+}
+
+
