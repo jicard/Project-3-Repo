8ff905963139cc9bd8eb4632145713301761cc03
jjohnson673 <jkjohnson673@gmail.com>
2023-03-01 18:57:48 -0500

    added in homepage base frame

--- client/src/components/homepage/homepage.js
+++ client/src/components/homepage/homepage.js
@@ -2,8 +2,29 @@ import React, {useState} from 'react'
 
 function homepage() {
   return (
-    <div>homepage</div>
-  )
+    <div className='homepage-container'>
+      <video src='/images/movingvideo.mp4' autoPlay loop muted />
+      <h1>MOVING?</h1>
+      <p>We Help You Stay Organized!</p>
+      <div className='hero-btns'>
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          GET STARTED
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('loginclick!')}
+        >
+          Login 
+        </Button>
+      </div>
+    </div>
+  );
 }
 
 export default homepage
