d95d0d7b4c8cccee2fc16c8ded4ec15852d2bb3d
Josh Icard <joshicard@Joshs-MacBook-Pro.local>
2023-03-02 18:55:45 -0500

    redeploy

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import List from './components/list'
 import TheFooter from './components/Footer'
 import Navbar from './components/Navbar'
-
 import Homepage from './components/NavbarPages/homepage'
 import About from './components/NavbarPages/about'
 import listButton from './components/NavbarPages/listButton'
@@ -19,7 +18,7 @@ function App() {
         <Routes>
           <Route path='/' exact component={Homepage} />
           <Route path='/about' exact component={About} />
-          <Route path='/aboutus' exact component={ContactUs} />
+          {/*<Route path='/aboutus' exact component={ContactUs} />*/}
       </Routes>
     </Router>
   );
