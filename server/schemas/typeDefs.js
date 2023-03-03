4edbc481446d078cede862d0727175b28e0de748
marysgreenwood <marysgreenwood@gmail.com>
2023-03-03 15:28:41 -0500

    connected Apollo server and database to server.js, added setUserInfo mutation

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,17 +2,15 @@ const { gql } = require("apollo-server-express");
 //const { Date } = require("graphql-scalars");
 
 const typeDefs = gql`
- 
   type User {
     _id: ID!
     username: String!
     email: String!
     password: String!
     currentAddress: String
-    destinationZip: String
-    movingDate: Date
+    destinationAddress: String
+    movingDate: String
     lists: [Lists]
-    
   }
 
   input ListItemInput {
@@ -24,11 +22,9 @@ const typeDefs = gql`
   type Lists {
     listName: String!
     listUser: String!
-    listItems: [ListItem]
-    
+    listItems: [String]
   }
 
-  
   type Auth {
     token: ID!
     user: User
@@ -37,17 +33,17 @@ const typeDefs = gql`
   type Query {
     users: User
     me: User
-
   }
 
   type Mutation {
     addUser(username: String!, email: String!, password: String!): Auth
     login(email: String!, password: String!): Auth
-    addList (listName: String!): Lists
-    addListItem(input: ListItemInput ): Lists
-    updateListItem (id: ID!, input: ListItemInput ): Lists
-    removeList (listId: ID!): Lists
-
+    setUserInfo (username: String, email: String, password: String, currentAddress: String, destinationAddress: String, movingDate: String): User
+    addList(listName: String!): Lists
+    addListItem(input: ListItemInput): Lists
+    updateListItem(id: ID!, input: ListItemInput): Lists
+    removeList(listId: ID!): Lists
+    removeListItem(listId: ID!, listItemId: ID!): Lists
   }
 `;
 
