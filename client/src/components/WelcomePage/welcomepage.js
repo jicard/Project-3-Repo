3b6fea3a1051842b9a22f0a571ac23078581ff38
jjohnson673 <jkjohnson673@gmail.com>
2023-03-04 12:54:16 -0500

    got video to work

--- client/src/components/WelcomePage/welcomepage.js
+++ client/src/components/WelcomePage/welcomepage.js
@@ -3,36 +3,35 @@ import styled from 'styled-components'
 import {motion} from 'framer-motion'
 import Button from '../Button/Button';
 import './welcomepage.css';
-// import MP4 from 'C:/Users/Julia J/Desktop/Project-3-Repo/client/public/images/movingvideo.mp4'
+import GIF from '../../images/moving.gif'
 
 const WelcomePage = () => {
   return (
     <div className='homepage-container'>
-              <video width="100%" height="50%" autoPlay loop muted controls >
-      <source src='C:\Users\Julia J\Desktop\Project-3-Repo\client\public\images\moving.gif' type="video/gif"/>
-</video>
-      <h1>MOVING?</h1>
-      <p>We Help You Stay Organized!</p>
-      <div className='hero-btns'>
-        <Button
-          className='btns'
-          buttonStyle='btn--outline'
-          buttonSize='btn--large'
-        >
-          GET STARTED
-        </Button>
-        <Button
-          className='btns'
-          buttonStyle='btn--primary'
-          buttonSize='btn--large'
-          onClick={console.log('loginclick!')}
-        >
-          Login 
-        </Button>
 
+      <img src={GIF} alt="Moving Truck" />
+      <div className= 'main-info'>
+        <h1>MOVING?</h1>
+        <p>We Help You Stay Organized!</p>
+        <div className='hero-btns'>
+          <Button
+            className='btns'
+            buttonStyle='btn--outline'
+            buttonSize='btn--large'
+          >
+            GET STARTED
+          </Button>
+          <Button
+            className='btns'
+            buttonStyle='btn--primary'
+            buttonSize='btn--large'
+            onClick={console.log('loginclick!')}
+          >
+            Login 
+          </Button>
 
-        {/* <video src= {MP4} autoPlay loop muted></video>  */}
-      </div>
+        </div>
+      </div>    
     </div>
   );
 };
