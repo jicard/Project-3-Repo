c31a3a308857c80c1a7b0ac2dfd7551834a132de
jjohnson673 <jkjohnson673@gmail.com>
2023-03-05 15:24:07 -0500

    added basic styling to page, still have more kinks to work out

--- client/src/components/AccountSettings/accountsettings.js
+++ client/src/components/AccountSettings/accountsettings.js
@@ -8,14 +8,23 @@ const accountsettings = () => {
     <div className='homepage-container'>
       <div className='moving-details'>
         <h1>Moving details</h1>
-        <h2>New address</h2>
-        <text>New address here</text>
-        <h2>Move-in Date:</h2>
-        <text>move-in date here</text>
+        <h2>New address</h2> <text>New address here</text>
+        <h2>Move-in Date:</h2> <text>move-in date here</text>
         <h2>Old Residence</h2>
         <text>old address here</text>
         <h2>Move Out Date:</h2>
         <text>date need to move out by</text>
+
+        <div className='hero-btns'>
+            <Button
+                className='btns'
+                buttonStyle='btn--primary'
+                buttonSize='btn--large'
+                onClick={console.log('loginclick!')}
+                >
+            Save 
+            </Button>
+        </div>
       </div>
       <div className= 'account-settings'>
         <h1>Account Settings</h1>
