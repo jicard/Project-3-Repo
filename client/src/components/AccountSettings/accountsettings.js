60c17a7f6f35807d81a02b6e7acf544b160bcb8f
jjohnson673 <jkjohnson673@gmail.com>
2023-03-05 15:58:57 -0500

    added more styling, more still needs to be done

--- client/src/components/AccountSettings/accountsettings.js
+++ client/src/components/AccountSettings/accountsettings.js
@@ -8,14 +8,23 @@ const accountsettings = () => {
     <div className='homepage-container'>
       <div className='moving-details'>
         <h1>Moving details</h1>
-        <h2>New address</h2> <text>New address here</text>
-        <h2>Move-in Date:</h2> <text>move-in date here</text>
-        <h2>Old Residence</h2>
+        <h3>New address</h3>
+        <text>New address here</text>
+        <h3>Move-in Date:</h3>
+        <text>move-in date here</text>
+        <h3>Old Residence</h3>
         <text>old address here</text>
-        <h2>Move Out Date:</h2>
+        <h3>Move Out Date:</h3>
         <text>date need to move out by</text>
 
-        <div className='hero-btns'>
+        <div className='account-btns'>
+            <Button
+                className='btns'
+                buttonStyle='btn--outline'
+                buttonSize='btn--large'
+            >
+                Edit
+            </Button>
             <Button
                 className='btns'
                 buttonStyle='btn--primary'
@@ -28,23 +37,30 @@ const accountsettings = () => {
       </div>
       <div className= 'account-settings'>
         <h1>Account Settings</h1>
-        <h2>name</h2>
+        <h3>name</h3>
         <text>name here</text>
-        <h2>email</h2>
+        <h3>email</h3>
         <text>emailhere</text>
-        <h2>password</h2>
+        <h3>password</h3>
         <text>password here</text>
 
-        <div className='hero-btns'>
+        <div className='account-btns'>
 
-          <Button
-            className='btns'
-            buttonStyle='btn--primary'
-            buttonSize='btn--large'
-            onClick={console.log('loginclick!')}
+        <Button
+          className='btns'
+          buttonStyle='btn--outline'
+          buttonSize='btn--large'
+        >
+          Edit
+        </Button>
+        <Button
+          className='btns'
+          buttonStyle='btn--primary'
+          buttonSize='btn--large'
+          onClick={console.log('loginclick!')}
           >
             Save 
-          </Button>
+        </Button>
         </div>
 
       </div>
