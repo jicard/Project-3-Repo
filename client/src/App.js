879caf7db07ac573b685faf223c2dd89b72c0c9a
Michael Smitte <smittemichael@yahoo.com>
2023-03-06 14:12:10 -0500

    fixed errors with app.js and index.js merging

--- client/src/App.js
+++ client/src/App.js
@@ -7,19 +7,8 @@ import List from './components/list/index'
 import TheFooter from './components/footer/index'
 import Navbar from './components/Navbar'
 import About from './components/NavbarPages/about'
-import listButton from './components/NavbarPages/listButton'
 // import ContactUs from './components/FooterPages/contactUs'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css';
-import AccountSettings from './components/AccountSettings/accountsettings';
-import TheFooter from './components/footer/index';
-import contactUs from './components/FooterPages/contactUs';
-import Lists from './components/list/index';
-import Navbar from './components/Navbar';
-import About from './components/NavbarPages/about';
-import Homepage from './components/NavbarPages/homepage';
-import listButton from './components/NavbarPages/listButton';
-import WelcomePage from './components/WelcomePage/welcomepage';
+
 
 function App() {
   return (
--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
