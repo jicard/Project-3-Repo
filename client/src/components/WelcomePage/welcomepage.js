0c9353a58852069922dc4975e9c7adb60acffb70
Josh Icard <111021673+jicard@users.noreply.github.com>
2023-03-06 21:32:18 -0500

    remove useEffect

--- client/src/components/WelcomePage/welcomepage.js
+++ client/src/components/WelcomePage/welcomepage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 // import styled from 'styled-components'
 // import { motion } from 'framer-motion'
 // import Button from '../Button/Button';
