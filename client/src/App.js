a9c13780b1c55c96e7f7c43b9ebf5a77c432c52f
Josh Icard <111021673+jicard@users.noreply.github.com>
2023-03-22 19:57:47 -0400

    heroku

--- client/src/App.js
+++ client/src/App.js
@@ -19,7 +19,7 @@ import {
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = createHttpLink({
-  uri: "http://localhost:3001/graphql",
+  uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
-const db = require("./config/connection");
+const db = require("config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
