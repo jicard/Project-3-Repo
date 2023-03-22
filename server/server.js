e6344c03438dd76a5eda1235c309bfd42430313a
Josh Icard <111021673+jicard@users.noreply.github.com>
2023-03-22 19:43:01 -0400

    heroku

--- server/server.js
+++ server/server.js
@@ -5,10 +5,8 @@ const { ApolloServer } = require("apollo-server-express");
 const { authMiddleware } = require("./utils/auth");
 const { typeDefs, resolvers } = require("./schemas");
 const db = require("./config/connection");
-//const cors = require('cors');
 
 const app = express();
-//app.use('*', cors({origin: 'http://localhost:3000'}))
 const PORT = process.env.PORT || 3001;
 
 const server = new ApolloServer({
